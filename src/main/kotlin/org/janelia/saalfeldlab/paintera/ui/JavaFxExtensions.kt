ad3aac0cbd869feb57b735eac8ab99a9d2bf77b6
Caleb Hulbert <cmhulbert@gmail.com>
2025-01-03 13:26:30 -0500

    feat: support customMapper for custom text formatter

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/ui/JavaFxExtensions.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/ui/JavaFxExtensions.kt
@@ -1,26 +1,34 @@
 package org.janelia.saalfeldlab.paintera.ui
 
 import javafx.scene.Node
-import javafx.scene.control.TextField
 import javafx.scene.control.TextFormatter
 import javafx.scene.layout.HBox.setHgrow
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox.setVgrow
 import javafx.util.StringConverter
-import net.imglib2.algorithm.math.ImgMath.let
-import org.jdom2.filter.Filters.text
 
 
-class PositiveLongTextFormatter(initialValue: Long? = null) : TextFormatter<Long?>(
+class PositiveLongTextFormatter(initialValue: Long? = null, customMapping: ((String?) -> String?)? = null) : TextFormatter<Long?>(
 	object : StringConverter<Long?>() {
 		override fun toString(`object`: Long?) = `object`?.takeIf { it >= 0 }?.let { "$it" }
 		override fun fromString(string: String?) = string?.toLongOrNull()
 	},
 	initialValue,
-	{ it.apply { if (controlNewText.runCatching{toLong()}.isFailure) text = "" } }
+	{
+		it.apply {
+			customMapping?.invoke(controlNewText)?.let { mapped ->
+				setRange(0, controlText.length)
+				text = mapped.takeIf { it == "" || it.runCatching { toLong() }.isSuccess } ?: ""
+			} ?: let {
+				if (controlNewText.runCatching { toLong() }.isFailure)
+					text = ""
+			}
+		}
+		it
+	}
 )
 
-class PositiveDoubleTextFormatter(initialValue: Double? = null, format : String? = "%.3f") : TextFormatter<Double>(
+class PositiveDoubleTextFormatter(initialValue: Double? = null, format: String? = "%.3f") : TextFormatter<Double>(
 	object : StringConverter<Double>() {
 		override fun toString(`object`: Double?) = `object`?.takeIf { it >= 0 }?.let { format?.format(it) ?: "$it" }
 		override fun fromString(string: String?) = string?.toDoubleOrNull()
